784ba84b267428ae8cadc6f09a77316f0d1b4f32
Richard Ettema <richard.ettema@aegis.net>
2024-07-16 09:15:57 -0400

    Remove incorrectly committed docker files

--- docker/wildfhirce/wildfhirce-mysql/wait_then_shutdown.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-while [ ! -f /tmp/server_can_shutdown.txt ] # <-- created by 99_last_processed_file.sh
-do
-  sleep 2
-done
-rm -f /tmp/server_can_shutdown.txt
-mysqladmin shutdown
-sleep 15
-
