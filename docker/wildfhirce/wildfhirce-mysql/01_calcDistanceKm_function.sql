1a430518384938d210d5891d3d8daa3572ee0c0f
Richard Ettema <richard.ettema@aegis.net>
2026-04-13 15:06:38 -0400

    Docker update to MySQL 9.6.0 base image

--- docker/wildfhirce/wildfhirce-mysql/01_calcDistanceKm_function.sql
+++ docker/wildfhirce/wildfhirce-mysql/01_calcDistanceKm_function.sql
@@ -1,21 +1,14 @@
-USE wildfhirr4
+USE wildfhirr4;
 DELIMITER $$
 
-create function calcDistanceKm(lat float, lng float, pnt_lat float, pnt_lng float)
+CREATE FUNCTION calcDistanceKm(lat float, lng float, pnt_lat float, pnt_lng float)
 
-Returns double DETERMINISTIC
+RETURNS double
+DETERMINISTIC
 BEGIN
 
-Declare dist double;
-SET dist =
-  6371 * acos (
-  cos ( radians(pnt_lat) )
-  * cos( radians( lat ) )
-  * cos( radians( lng ) - radians(pnt_lng) )
-  + sin ( radians(pnt_lat) )
-  * sin( radians( lat ) )
-);
+  RETURN 6371 * acos ( cos ( radians(pnt_lat) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(pnt_lng) ) + sin ( radians(pnt_lat) ) * sin( radians( lat ) ));
 
-RETURN dist;
+END$$
 
-END
+DELIMITER ;
